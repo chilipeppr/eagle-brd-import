0f8dd28956a91623b24ddf3a67e519cde3d1eab8
John Lauer <jlauer@chilipeppr.com>
2015-12-17 05:56:33 +0000

    Check-in of css and js files

---
+++ nodetestserver.js
@@ -0,0 +1,73 @@
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
+
+http.createServer(function (req, res) {
+  
+  var uri = url.parse(req.url).pathname;
+  var filename = path.join(process.cwd(), unescape(uri));
+  var stats;
+
+  try {
+    stats = fs.lstatSync(filename); // throws if path doesn't exist
+  } catch (e) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('404 Not Found\n');
+    res.end();
+    return;
+  }
+  
+  if (stats.isFile()) {
+    // path exists, is a file
+    var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+    res.writeHead(200, {'Content-Type': mimeType} );
+
+    var fileStream = fs.createReadStream(filename);
+    fileStream.pipe(res);
+  } else if (uri == "/") {
+    
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    var widgetUrl = 'http://' +
+     process.env.C9_PROJECT + '-' +  process.env.C9_USER +
+     '.c9users.io/widget.html';
+    var editUrl = 'http://ide.c9.io/' +
+     process.env.C9_USER + '/' +
+     process.env.C9_PROJECT;
+    
+    res.end(
+      '<html><body>' +
+      'Your ChiliPeppr Widget can be tested at ' +
+      '<a href="' + widgetUrl + '">' +
+      widgetUrl + '</a><br><br>\n\n' + 
+      'Your ChiliPeppr Widget can be edited at ' +
+      '<a href="' + editUrl + '">' +
+      editUrl + '</a><br><br>\n\n' + 
+    'C9_PROJECT: ' + process.env.C9_PROJECT + '<br>\n' +
+    'C9_USER: ' + process.env.C9_USER + '\n' +
+    '');
+    
+  } else if (stats.isDirectory()) {
+    // path exists, is a directory
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Index of '+uri+'\n');
+    res.write('TODO, show index?\n');
+    res.end();
+  } else {
+    // Symbolic link, other?
+    // TODO: follow symlinks?  security?
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.write('500 Internal server error\n');
+    res.end();
+  }
+  
+  
+}).listen(process.env.PORT);
---
+++ widget.js
@@ -0,0 +1,1 @@
+// Javascript for ChiliPeppr Widget
