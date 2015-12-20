ca49c675b761b5cd1f01aa76d64d44f9ce4f28df
John Lauer <jlauer@chilipeppr.com>
2015-12-20 19:26:56 +0000

    Made some changes to widget using Cloud9

--- git-push.sh
+++ git-push.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# In Cloud9 IDE, right-click and choose "Run" on this file to push your changes to 
+# Github. The Cloud9 IDE will run this on the virtual machine command line to
+# trigger the push to Github. Of course you can manually run these commands too
+# if you want to push your changes in a different way to Github.
 git add *
-git commit
+git commit -m "Made some changes to widget using Cloud9"
 git push
